5af48b6ac630f2c67e886c3670cdcdc337500836
gaolinfeng <gaolinfeng@kaike.la>
2021-06-27 19:27:41 +0800

    异常处理

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let data = [];
 let listIndex = 0;
 const total = 250;
 const saveFileName = 'movieTop250.json';
-const Cookie = 'dbcl2=""'; // dbcl2值，浏览器登录douban后从cookie获取，必须设置，否则会被豆瓣拦截
+const Cookie = 'dbcl2="xxx"'; // dbcl2值，浏览器登录douban后从cookie获取，必须设置，否则会被豆瓣拦截
 
 // 影片信息对象类定义
 class MovieObj {
@@ -32,33 +32,39 @@ class MovieObj {
  */
 async function getList() {
   let url = `https://movie.douban.com/top250?start=${listIndex}&filter=`;
-  let res = await axios({
-    url,
-    headers: {
-      Cookie,
-    },
-  }).catch((err) => {
+  try {
+    let res = await axios({
+      url,
+      headers: {
+        Cookie,
+      },
+    });
+    let $ = cheerio.load(res.data, {
+      xml: {
+        normalizeWhitespace: true,
+      },
+    });
+    let list = $('.grid_view .item .pic a');
+    for (let i = 0, len = list.length; i < len; i++) {
+      let url = $(list[i]).attr('href');
+      let index = i + listIndex + 1;
+      console.log('处理进度', index + '/' + total);
+      try {
+        await getPage(url);
+        console.log('已完成', Number((index / total) * 100).toFixed(1) + '%');
+      } catch (err) {
+        console.log(err);
+        return;
+      }
+    }
+    listIndex += list.length;
+    if (listIndex < total) {
+      getList();
+    } else {
+      saveData();
+    }
+  } catch (err) {
     console.log(err);
-    return;
-  });
-  let $ = cheerio.load(res.data, {
-    xml: {
-      normalizeWhitespace: true,
-    },
-  });
-  let list = $('.grid_view .item .pic a');
-  for (let i = 0, len = list.length; i < len; i++) {
-    let url = $(list[i]).attr('href');
-    let index = i + listIndex + 1;
-    console.log('处理进度', index + '/' + total);
-    await getPage(url);
-    console.log('已完成', Number((index / total) * 100).toFixed(1) + '%');
-  }
-  listIndex += list.length;
-  if (listIndex < total) {
-    getList();
-  } else {
-    saveData();
   }
 }
 
@@ -67,74 +73,76 @@ async function getList() {
  * @param {*} url
  */
 function getPage(url) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     setTimeout(async () => {
       console.log(url);
-      let res = await axios({
-        url,
-        headers: {
-          Cookie,
-        },
-      }).catch((err) => {
-        console.log(err);
-      });
-      let $ = cheerio.load(res.data, {
-        xml: {
-          normalizeWhitespace: true,
-        },
-      });
-      console.log($);
-      let movieObj = new MovieObj(url);
-      // 标题
-      movieObj.title = $('[property="v:itemreviewed"]').text();
-      // 年份
-      movieObj.year = +$('.year')
-        .text()
-        .match(/\((.*)\)/)[1];
-      // 封面
-      movieObj.picUrl = $('[rel="v:image"]').attr('src');
-      // 排名
-      movieObj.topNo = +$('.top250-no').text().split('.')[1];
-      // 评分
-      movieObj.score = +$('[property="v:average"]').text();
-      // 评分数
-      movieObj.votes = +$('[property="v:votes"]').text();
-      // 片长
-      movieObj.duration = +$('[property="v:runtime"]').attr('content');
-      // 可播放状态
-      let onlinePlayStatus = $('.buylink-price').text().match(/\S+/g);
-      if (onlinePlayStatus) {
-        let isFree = onlinePlayStatus.find((item) => item == '免费观看');
-        if (isFree) {
-          movieObj.playType = 1;
-        } else {
-          movieObj.playType = 2;
+      try {
+        let res = await axios({
+          url,
+          headers: {
+            Cookie,
+          },
+        });
+        let $ = cheerio.load(res.data, {
+          xml: {
+            normalizeWhitespace: true,
+          },
+        });
+        console.log($);
+        let movieObj = new MovieObj(url);
+        // 标题
+        movieObj.title = $('[property="v:itemreviewed"]').text();
+        // 年份
+        movieObj.year = +$('.year')
+          .text()
+          .match(/\((.*)\)/)[1];
+        // 封面
+        movieObj.picUrl = $('[rel="v:image"]').attr('src');
+        // 排名
+        movieObj.topNo = +$('.top250-no').text().split('.')[1];
+        // 评分
+        movieObj.score = +$('[property="v:average"]').text();
+        // 评分数
+        movieObj.votes = +$('[property="v:votes"]').text();
+        // 片长
+        movieObj.duration = +$('[property="v:runtime"]').attr('content');
+        // 可播放状态
+        let onlinePlayStatus = $('.buylink-price').text().match(/\S+/g);
+        if (onlinePlayStatus) {
+          let isFree = onlinePlayStatus.find((item) => item == '免费观看');
+          if (isFree) {
+            movieObj.playType = 1;
+          } else {
+            movieObj.playType = 2;
+          }
         }
+        // 类型
+        movieObj.genres = $('[property="v:genre"]')
+          .map((index, item) => $(item).text())
+          .get();
+        // 地区
+        let areaStr = $('span:contains("地区:")')[0].next.data;
+        movieObj.areas = areaStr.match(/\S+/g).join('').split('/');
+        // 语言
+        let langStr = $('span:contains("语言:")')[0].next.data;
+        movieObj.languages = langStr.match(/\S+/g).join('').split('/');
+        // 导演
+        movieObj.directors = $('[rel="v:directedBy"]')
+          .map((index, item) => $(item).text())
+          .get();
+        // 编剧
+        movieObj.writers = $('span:contains("编剧")')
+          .next()
+          .children()
+          .map((index, item) => $(item).text())
+          .get();
+        // 输出
+        data.push(movieObj);
+        console.log(movieObj);
+        resolve();
+      } catch (err) {
+        reject(err);
       }
-      // 类型
-      movieObj.genres = $('[property="v:genre"]')
-        .map((index, item) => $(item).text())
-        .get();
-      // 地区
-      let areaStr = $('span:contains("地区:")')[0].next.data;
-      movieObj.areas = areaStr.match(/\S+/g).join('').split('/');
-      // 语言
-      let langStr = $('span:contains("语言:")')[0].next.data;
-      movieObj.languages = langStr.match(/\S+/g).join('').split('/');
-      // 导演
-      movieObj.directors = $('[rel="v:directedBy"]')
-        .map((index, item) => $(item).text())
-        .get();
-      // 编剧
-      movieObj.writers = $('span:contains("编剧")')
-        .next()
-        .children()
-        .map((index, item) => $(item).text())
-        .get();
-      // 输出
-      data.push(movieObj);
-      console.log(movieObj);
-      resolve();
     }, 1000);
   });
 }
